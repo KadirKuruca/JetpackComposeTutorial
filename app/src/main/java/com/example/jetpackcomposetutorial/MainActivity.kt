8557d5d8e1e3bbb01af35eb8a96193685cc03e7b
Kadir Kuruca <kadir.kuruca@migrosonline.com>
2022-09-02 15:31:21 +0300

    Adding simple text composables

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BirthdayGreetingWithText(
+                        message = "Happy Birthday Android",
+                        from = "- From Kadir"
+                    )
                 }
             }
         }
@@ -30,14 +36,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BirthdayGreetingWithText(message: String, from: String){
+    Column{
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DefaultPreview() {
-    JetpackComposeTutorialTheme {
-        Greeting("Android")
+fun BirthdayCardPreview(){
+    JetpackComposeTutorialTheme{
+        BirthdayGreetingWithText("Happy Birthday Android", "- From Kadir")
     }
 }
