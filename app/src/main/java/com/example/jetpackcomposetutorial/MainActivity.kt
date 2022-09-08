45827e5296ebae18c08d62dc78b64f8a6aebfd5f
Kadir Kuruca <kadir.kuruca@migrosonline.com>
2022-09-08 21:20:13 +0300

    Create a Lemonade App ui

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -4,18 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
@@ -30,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    DiceRollerApp()
+                    LemonadeApp()
                 }
             }
         }
@@ -38,35 +40,87 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier){
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when(result){
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        6 -> R.drawable.dice_6
-        else -> {R.drawable.dice_1}
+fun LemonadeScreen() {
+    var currentStep by remember { mutableStateOf(1) }
+    var randomSqueezeCount by remember { mutableStateOf(0) }
+    when (currentStep) {
+        1 -> {
+            LemonWithImageAndText(
+                R.string.tap_to_select_lemon_text,
+                R.drawable.lemon_tree,
+                R.string.lemon_tree_content_description_text,
+                onImageClick = {
+                    currentStep = 2
+                    randomSqueezeCount = (2..6).random()
+                }
+            )
+        }
+        2 -> {
+            LemonWithImageAndText(
+                R.string.tap_to_squeeze_lemon_text,
+                R.drawable.lemon_squeeze,
+                R.string.lemon_content_description_text,
+                onImageClick = {
+                    randomSqueezeCount--
+                    if(randomSqueezeCount == 0)
+                        currentStep = 3
+                }
+            )
+        }
+        3 -> {
+            LemonWithImageAndText(
+                R.string.tap_lemon_to_drink_text,
+                R.drawable.lemon_drink,
+                R.string.glass_of_lemonade_content_description_text,
+                onImageClick = {
+                    currentStep = 4
+                }
+            )
+        }
+        4 -> {
+            LemonWithImageAndText(
+                R.string.tap_glass_to_start_again_text,
+                R.drawable.lemon_restart,
+                R.string.empty_glass_content_description_text,
+                onImageClick = {
+                    currentStep = 1
+                }
+            )
+        }
     }
+}
+
+@Composable
+fun LemonWithImageAndText(
+    textId: Int,
+    imageId: Int,
+    contentDescIt: Int,
+    onImageClick: () -> Unit
+) {
     Column(
-        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text(text = stringResource(id = R.string.roll_text))
-        }
+        Text(
+            text = stringResource(id = textId),
+            fontSize = 16.sp
+        )
+        Image(
+            painter = painterResource(id = imageId),
+            contentDescription = stringResource(id = contentDescIt),
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .border(1.dp, Color(0xFF69cdd8), CutCornerShape(4.dp))
+                .wrapContentSize()
+                .clickable(
+                    onClick = onImageClick
+                )
+        )
     }
 }
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
-    )
+fun LemonadeApp() {
+    LemonadeScreen()
 }
