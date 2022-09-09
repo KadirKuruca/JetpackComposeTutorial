e982c46590c9ce86548f4d020114f7a5b91e4774
Kadir Kuruca <kadir.kuruca@migrosonline.com>
2022-09-09 22:37:46 +0300

    Create a CalculateTip App

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -3,24 +3,25 @@ package com.example.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,95 +33,68 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    LemonadeApp()
+                    TipCalculatorApp()
                 }
             }
         }
     }
 }
 
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0
+): String{
+    val tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
 @Composable
-fun LemonadeScreen() {
-    var currentStep by remember { mutableStateOf(1) }
-    var randomSqueezeCount by remember { mutableStateOf(0) }
-    when (currentStep) {
-        1 -> {
-            LemonWithImageAndText(
-                R.string.tap_to_select_lemon_text,
-                R.drawable.lemon_tree,
-                R.string.lemon_tree_content_description_text,
-                onImageClick = {
-                    currentStep = 2
-                    randomSqueezeCount = (2..6).random()
-                }
-            )
-        }
-        2 -> {
-            LemonWithImageAndText(
-                R.string.tap_to_squeeze_lemon_text,
-                R.drawable.lemon_squeeze,
-                R.string.lemon_content_description_text,
-                onImageClick = {
-                    randomSqueezeCount--
-                    if(randomSqueezeCount == 0)
-                        currentStep = 3
-                }
-            )
-        }
-        3 -> {
-            LemonWithImageAndText(
-                R.string.tap_lemon_to_drink_text,
-                R.drawable.lemon_drink,
-                R.string.glass_of_lemonade_content_description_text,
-                onImageClick = {
-                    currentStep = 4
-                }
-            )
-        }
-        4 -> {
-            LemonWithImageAndText(
-                R.string.tap_glass_to_start_again_text,
-                R.drawable.lemon_restart,
-                R.string.empty_glass_content_description_text,
-                onImageClick = {
-                    currentStep = 1
-                }
-            )
-        }
+fun TipCalculatorPage() {
+    var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.calculate_tip),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Normal
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(id = R.string.tip_amount, tip),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
 @Composable
-fun LemonWithImageAndText(
-    textId: Int,
-    imageId: Int,
-    contentDescIt: Int,
-    onImageClick: () -> Unit
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit
 ) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = stringResource(id = textId),
-            fontSize = 16.sp
-        )
-        Image(
-            painter = painterResource(id = imageId),
-            contentDescription = stringResource(id = contentDescIt),
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .border(1.dp, Color(0xFF69cdd8), CutCornerShape(4.dp))
-                .wrapContentSize()
-                .clickable(
-                    onClick = onImageClick
-                )
-        )
-    }
+    TextField(
+        value = value,
+        label = { Text(text = stringResource(id = R.string.cost_of_service)) },
+        singleLine = true,
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = onValueChange,
+    )
 }
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun LemonadeApp() {
-    LemonadeScreen()
+fun TipCalculatorApp() {
+    TipCalculatorPage()
 }
